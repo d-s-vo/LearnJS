7667f04283216cd7ee725a89f9c85fcf8b11bbce
Dmitry <d.smirnov.vo@mail.ru>
2022-04-29 09:22:07 +0300

    start first practice in script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-" use strict ";
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const user = "Dmitriy";
-alert(`Hello, ${user}`);
-  
+const personaMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ""),
+      b = prompt('На сколько оцените его?', "");
+      c = prompt("Whos of actor you like mostly?", '');
+      d = prompt('How would you rate him?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personaMovieDB);
