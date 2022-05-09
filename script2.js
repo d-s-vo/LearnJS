06ca6993d39beff00883c0ff48309b10ba0303a1
Dmitry <d.smirnov.vo@mail.ru>
2022-05-09 13:48:52 +0300

    script3.js все циклы обернули в функции

--- script2.js
+++ script2.js
@@ -1,1 +1,14 @@
 "use strict"; 
+ 
+function showFirstMessage (jepa){
+    console.log(jepa);
+}
+showFirstMessage('Hello, DUDES!');
+
+function ret () {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret ();
+console.log(anotherNum);
---
+++ script3.js
@@ -0,0 +1,57 @@
+"use strict";
+
+
+let numberOfFilms;
+
+function start () {
+   numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+   while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+    numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+   }
+}
+start();
+
+const personaMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+
+function checkWatchedMovies () {
+    for (let i = 1; i < 2; i++){
+        const a = prompt('Один из последних просмотренных фильмов?', ""),
+              b = prompt('На сколько оцените его?', "");
+              
+        if (a != '' && b != '' && a != null && b != null && a.length < 50){
+              personaMovieDB.movies[a] = b;
+              console.log('все верно!');
+        } else {
+          console.log('error');
+          i--;
+        }     
+      }
+}
+checkWatchedMovies ();
+
+
+function checkMovieLevel () {
+    if (personaMovieDB.count < 10){
+        console.log('слабак хуев');
+      } else if (personaMovieDB.count >= 10 && personaMovieDB.count < 30){
+        console.log('не самый лошара');
+      } else if ( personaMovieDB.count > 30) {
+        console.log('трушный чел');
+      } 
+}
+checkMovieLevel ();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personaMovieDB);
+    } 
+}
+showMyDB(personaMovieDB.private);
+
