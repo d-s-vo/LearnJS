7ad2b49afa486dd49add87a8e2f3fcc5d97a14a0
Dmitry <d.smirnov.vo@mail.ru>
2022-05-12 12:28:33 +0300

    script4.js методы объекта и деструктуририющее присваивание

---
+++ script4.js
@@ -0,0 +1,29 @@
+"use strict";
+ 
+const jepa = {
+    name: 'test',
+    width: 90,
+    colors: {
+        border: 'black',
+        bg: 'green'
+    }
+};
+
+console.log(Object.keys(jepa).length);
+
+const {border, bg} = jepa.colors; 
+console.log(border);
+
+/* let counter = 0;
+for (let key in jepa) {
+    if (typeof  (jepa[key]) === "object") {
+        for (let i in jepa[key]){
+            console.log(`Свойство ${i} имеет значение ${jepa[key][i]}`);
+            counter++; 
+        } 
+    }else {
+        console.log(`Свойство ${key} имеет значение ${jepa[key]}`);
+        counter++;
+    }
+}
+console.log(counter); */
