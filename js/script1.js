448a5a501677f34a0aa35df1e8b5d6d44156dde7
Dmitry <d.smirnov.vo@mail.ru>
2022-07-31 23:42:48 +0300

    разобрал различие атрибутов async и defer/ динамически подгрузил скрипт5 через функцию в скрипт1

---
+++ js/script1.js
@@ -0,0 +1,12 @@
+"use strict";
+
+
+
+function createScript (src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    document.body.append(script);
+}
+
+createScript('js/script5.js');
