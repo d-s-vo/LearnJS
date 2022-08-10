a8a6d645e7e8fce2e0139901ab9b45cd2e3b0e62
Dmitry <d.smirnov.vo@mail.ru>
2022-08-10 17:38:00 +0300

    не работающее сохранение

--- js/script5.js
+++ js/script5.js
@@ -4,20 +4,23 @@ const btns = document.querySelectorAll('button'),
       wrapper = document.querySelector(".btn-block");
 const list = document.createElement('ul'),
       info = document.createElement('p');
+const tabPic = document.querySelectorAll('.divImage'),
+      tabWord = document.querySelectorAll('.ulPicture'),
+      tabParent = document.querySelector('.conteiner');
 
 info.textContent = "Я заебался уже сегодня тыкать в ебучий код чтобы кнопки заработали";     
 
 /* document.body.appendChild(info); 
 document.body.appendChild(list); */
 
-const nodeLi = document.createElement('li');
+
 const brtn = document.createElement('button');
 brtn.classList.add('button1');
-brtn.textContent = 'enter please';
-nodeLi.appendChild(brtn);
-wrapper.prepend(nodeLi);
+brtn.textContent = 'choose';
 wrapper.prepend(info);
+wrapper.append(brtn);
 
+/* делегирование функций с родителя на все кнопуи + динамечаски созданная кнопка brtn */
 wrapper.addEventListener('click', (e) => {
   console.log('fuckYou');
 
@@ -42,6 +45,40 @@ wrapper.addEventListener('click', (e) => {
         }  
     
 
+});
+
+/* включение табов на картинки и слова */
+
+function hideTabContent () {
+  tabPic.forEach (pic => {
+   pic.classList.add('hide');
+  });
+  tabWord.forEach (word => {
+    tabWord.classList.remove('ulPicture_item_active');
+  });
+}
+
+hideTabContent();
+
+function showTabContent (i = 0) {
+  tabPic[i].classList.add('show');
+  tabWord[i].classList.add('ulPicture_item_active');
+} 
+
+
+showTabContent(2); 
+
+tabParent.addEventListener ('click', (e) => {
+ const target = e.target;
+ if (target && target.classList.contains('ulPicture')){
+   tabWord.forEach((item, i) => {
+     if(target == item) {
+      hideTabContent();
+      showTabContent(i);
+     }
+   });
+ }
+ 
 });
 
 
